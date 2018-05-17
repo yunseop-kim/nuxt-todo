c6088de29218d9ec916b767e9c3701fa3171858d
Yunseop Kim <yunsub2@gmail.com>
2018-05-17 22:16:03 +0900

    fetch + vuex 를 위한 서버 소스

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
-
+let todoList = [{
+  idx: 1,
+  done: false,
+  text: "nuxt 공부"
+}];
 app.set('port', port)
 
 // Import and Set Nuxt.js options
@@ -20,6 +24,14 @@ async function start() {
     const builder = new Builder(nuxt)
     await builder.build()
   }
+
+  // read
+  app.get("/api/todo", (req, res, next) => {
+    console.log("todo List");
+    res.status(200).json({
+      text: "api test"
+    });
+  });
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
