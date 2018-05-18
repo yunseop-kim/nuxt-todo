9cf4a19138dc2171b328c5fb1dfadcaa258cf847
Yunseop Kim <yunsub2@gmail.com>
2018-05-18 09:11:58 +0900

    1. 편집 가능하도록 기능 구현

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,11 @@
   <div>
     <h4>{{$store.state.todos.test}}</h4>
     <ul>
-      <li v-for="todo in todos" :key="todo.text">
+      <li v-for="(todo, index) in todos" :key="todo.text">
         <input type="checkbox" :checked="todo.done" @change="toggle(todo)">
-        <span :class="{ done: todo.done }">{{ todo.text }}</span>
+        <input v-if="selected === index" type="text" v-model="editText" @keyup.enter="edit">
+        <span v-else :class="{ done: todo.done }" @click="selectToEdit(index, todo)">{{ todo.text }}</span>
+        <button>delete</button>
       </li>
       <li><input placeholder="What needs to be done?" @keyup.enter="addTodo"></li>
     </ul>
@@ -15,6 +17,12 @@
 import { mapMutations } from 'vuex'
 import axios from 'axios';
 export default {
+  data () {
+    return {
+      selected: -1,
+      editText: ""
+    }
+  },
   computed: {
     todos () { return this.$store.state.todos.list }
   },
@@ -26,6 +34,15 @@ export default {
     addTodo (e) {
       this.$store.commit('todos/add', e.target.value)
       e.target.value = ''
+    },
+    selectToEdit(index, todo) {
+      console.log("selectToEdit");
+      this.selected = index;
+      this.editText = todo.text;
+    },
+    edit(e) {
+      // this.$store.commit('todos/edit', e.target.value)
+      this.selected = -1
     },
     ...mapMutations({
       toggle: 'todos/toggle'
