02b2e7bd1c3aefa82051ca5424c5569226c2d42b
Yunseop Kim <yunsub2@gmail.com>
2018-05-18 15:21:19 +0900

    1. api : sequelize 연동

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-
 const express = require('express')
-const { Nuxt, Builder } = require('nuxt')
+const {
+  Nuxt,
+  Builder
+} = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
-let todoList = [{
-  idx: 1,
-  done: false,
-  text: "nuxt 공부"
-}];
+const models = require('../models')
+var routes = require('./routes/index');
+var todos = require('./routes/todos');
 app.set('port', port)
 
 // Import and Set Nuxt.js options
@@ -18,26 +18,19 @@ config.dev = !(process.env.NODE_ENV === 'production')
 async function start() {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
-
   // Build only in dev mode
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
   }
-
-  // read
-  app.get("/api/todo", (req, res, next) => {
-    console.log("todo List");
-    res.status(200).json({
-      text: "api test"
-    });
+  models.sequelize.sync().then(() => {
+    app.use('/api', routes)
+    app.use('/api/todos', todos)
+    // Give nuxt middleware to express
+    app.use(nuxt.render)
+    // Listen the server
+    app.listen(port, host)
+    console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
   });
-
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
-
-  // Listen the server
-  app.listen(port, host)
-  console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
 }
 start()
